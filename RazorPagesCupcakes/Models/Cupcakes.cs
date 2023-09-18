0a15310e289a16d2f7ea8cadc36ed0a6852ef980
Jessie Houghton <houghj@umich.edu>
2023-09-18 11:02:49 -0700

    Add project files.

---
+++ RazorPagesCupcakes/Models/Cupcakes.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RazorPagesCupcakes.Models;
+
+public class Cupcakes
+{
+    public int Id { get; set; }
+
+    [Required]
+    public string? Name { get; set; }
+    public bool IsGlutenFree { get; set; }
+
+    [Range(0.01, 9999.99)]
+    public decimal Price { get; set; }
+}
---
+++ RazorPagesCupcakes/Models/Distance.cs
@@ -0,0 +1,15 @@
+﻿namespace RazorPagesCupcakes.Models
+{
+    public readonly struct Distance
+    {
+        public readonly double Magnitude { get; }
+
+        public readonly double Direction { get; }
+
+        public Distance(double dx, double dy) // Use primary constructor
+        {
+            Magnitude = Math.Sqrt(dx * dx + dy * dy);
+            Direction = Math.Atan2(dy, dx);
+        }
+    }
+}
